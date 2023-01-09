4e8cf9a2930bc0b1cbe5346294cdb219e0826c8b
Sofía <114498817+sofiaffernandez@users.noreply.github.com>
2023-01-09 10:59:14 +0100

    Update editarUsuario.js

--- Controllers/Usuarios/editarUsuario.js
+++ Controllers/Usuarios/editarUsuario.js
@@ -52,53 +52,7 @@ async function editUser(req, res, next) {
       }
     } else {
       savedFileName = currentUser[0].image;
-    }
-
-    // Si el email es diferente al actual comprobar que no existe en la base de datos
-    if (email !== currentUser[0].email) {
-      const [existingEmail] = await connection.query(
-        `SELECT id
-        FROM usuarios
-        WHERE email=? `,
-        [email]
-      );
-
-      if (existingEmail.length > 0) {
-        throw generateError(
-          "Ya existe un usuario con este email en la base de datos",
-          403
-        );
-      }
-
-      // Verificamos de nuevo el email recibido
-      const registrationCode = randomString(40);
-      const validationURL = `${process.env.PUBLIC_HOST}/users/validate/${registrationCode}`;
-
-      //Enviamos la url anterior por mail
-      try {
-        await sendMail({
-          email,
-          title:
-            "Cambiaste tu email en la aplicación recomendaciones de viajes. Por favor valida de nuevo",
-          content: `Para validar tu nuevo email en la app recomendaciones de viajes haz click aquí: ${validationURL}`,
-        });
-      } catch (error) {
-        throw generateError("Error en el envío de mail", 500);
-      }
-
-      await connection.query(
-        `UPDATE usuario 
-        SET nombre=?, email=?, ultimo_cambio_contraseña=UTC_TIMESTAMP, active=false, codigo_validacion=?, avatar=?
-        WHERE id=?`,
-        [nombre, email, registrationCode, savedFileName, id]
-      );
-
-      // Dar una respuesta
-      res.send({
-        status: "ok",
-        message: "Usuario actualizado. Mira tu email para activarlo de nuevo.",
-      });
-    } else {
+   else {
       // Actualizar usuario en la base de datos
       await connection.query(
         `UPDATE usuarios 
